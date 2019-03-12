a673a64d08bf4ce6288669116f4cf40c9011fa97
arevenchuk <48455746+arevenchuk@users.noreply.github.com>
2019-03-12 04:57:51 +0200

    Update pg_backup.sh

--- pg_backup.sh
+++ pg_backup.sh
@@ -2,5 +2,17 @@
 
 BACKUP_DIR="`pwd`/`date +%F_%H-%M`"
 PSQL="psql -At postgres"
-tar -cvf zz.tar $(echo "SHOW config_file; SHOW ident_file; SHOW hba_file;"|$PSQL) 2>/dev/null
+
+mkdir $BACKUP_DIR
+
+echo "`date` Backup configuration files:"
+echo "[config]" >> $BACKUP_DIR/.backup.history
+(echo "SHOW config_file; SHOW ident_file; SHOW hba_file;"|$PSQL)|
+while read fn; do
+ cd ${fn%/*}
+ tar -rf $BACKUP_DIR/config_files.tar ${fn##*/} &&
+ echo $fn >> $BACKUP_DIR/.backup.history;
+ echo "`date` $fn"
+done
+
 exit 0
