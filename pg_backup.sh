3c287df9d45fa9614bcc14fedc23c97176ab07ce
arevenchuk <48455746+arevenchuk@users.noreply.github.com>
2019-03-13 03:46:39 +0200

    Update pg_backup.sh

--- pg_backup.sh
+++ pg_backup.sh
@@ -2,18 +2,22 @@
 
 BACKUP_DIR="`pwd`/`date +%F_%H-%M`"
 PSQL="psql -At postgres"
+TAR="tar -rf"
 
 mkdir $BACKUP_DIR
 if [ $? != 0 ]; then exit 1; fi
 
+# Add to backup files: postgresql.conf, pg_hba.conf, pg_ident.conf
 echo "`date` Backup configuration files:"
-echo "[config]" >> $BACKUP_DIR/.backup.history
-(echo "SHOW config_file; SHOW ident_file; SHOW hba_file;"|$PSQL)|
-while read fn; do
+echo "[config]" >> "$BACKUP_DIR"/.backup.history
+# (echo "SHOW config_file; SHOW ident_file; SHOW hba_file;"|$PSQL)|
+# while read fn; do
+for fn in $(echo "SHOW config_file; SHOW ident_file; SHOW hba_file;"|$PSQL) do
  cd ${fn%/*}
- tar -rf $BACKUP_DIR/config_files.tar ${fn##*/} &&
- echo $fn >> $BACKUP_DIR/.backup.history;
+ $TAR "$BACKUP_DIR"/config_files.tar ${fn##*/} &&
+ echo $fn >> "$BACKUP_DIR"/.backup.history;
  echo "`date` $fn"
 done
 
 exit 0
+
